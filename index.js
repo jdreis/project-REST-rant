22b8a4dcc93e2c71404c385d51e7e7d26a73a5aa
Jena Reis <jenadreis@gmail.com>
2022-02-11 15:49:51 -0500

    Adding places controller

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.use('/places', require('./controllers/places'))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.get('*', (req, res) => {
     res.status(404).send(
-        `<h1 style="text-align:center;margin-top:17vh;">404<br>Page Not Found</h1>`)
-})
+        `<h1 style="text-align:center;margin-top:17vh;">404<br>Page Not Found</h1>`);
+});
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
